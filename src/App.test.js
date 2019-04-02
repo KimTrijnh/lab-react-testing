391f0f176c32bf5ce688cf2a2e458e918abc62e7
Huong Trinh <trtrhuong@gmail.com>
2019-04-02 18:11:54 +0700

    milestone 2

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders the name of my site', () => {
+  const output = shallow(<App/>);
+  const header = output.find('h1').first();
+  expect(header.contains('My E-Commerce Site')).toBe(true);
+});
